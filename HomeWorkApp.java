fd6b0c6d3100c1d7b69330b148f5fa7967ca322d
Tanzilya89 <97829903+Tanzilya89@users.noreply.github.com>
2022-05-31 19:04:05 +0300

    Add files via upload

---
+++ HomeWorkApp.java
@@ -0,0 +1,52 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    public static void checkSumSign() {
+        int a = 6, b = 9, c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        if (c < 0) {
+            System.out.println("Сумма отрицательная");
+        }
+
+    public static void printColor() {
+        int value = 54;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value < 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+
+        }
+
+    public static void Numbers() {
+        int a = 6;
+        int b = 26;
+
+        if (a >=b ) {
+            System.out.println("a >=b" );
+        }
+        if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+}
+
+
+
+
+
